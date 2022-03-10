6da067115d3acd9187756a9216a2c7ed00276df1
aguang45 <88522807+aguang45@users.noreply.github.com>
2022-03-10 17:52:46 +0800

    Add files via upload

---
+++ test01.java
@@ -0,0 +1,73 @@
+package b_test01;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test01 {
+
+    public static void main(String[] args) {
+        /*Student a=new Student();
+        Scanner sc=new Scanner(System.in);
+        a.grade=sc.nextInt();
+
+        System.out.println(a.name+a.grade);
+        a.no_p();
+        a.name_p();*/
+
+     /*   Student s1=new Student("阿广");
+        Student s2=new Student(50);
+        Student s3=new Student("阿广",50);
+        s1.getName();s1.getNo();
+        s2.getName();s2.getNo();
+        s3.getName();s3.getNo();
+        s1.show();s2.show();s3.show();
+    */
+
+    /* Student s1=new Student("zhou",77);
+        Student s2=new Student("wen",88);
+        Student s3=new Student("guang",99);
+        ArrayList<Student> array=new ArrayList<>();
+        array.add(s1);
+        array.add(s2);
+        array.add(s3);
+        for(int i=0;i<array.size();i++){
+            Student s=array.get(i);
+            s.show();
+        }*/
+        //Scanner sc=new Scanner(System.in);
+        ArrayList<Student> array = new ArrayList<Student>();
+
+        /*Student s=new Student();
+        System.out.println("输入学生的姓名：");
+        s.SetName(sc.nextLine());
+        System.out.println("输入学生的成绩：");
+        s.SetGrade(sc.nextInt());
+        array.add(s);*/
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入学生的数目：");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            addStudent(array);
+        System.out.println(" ");
+        for (int i = 0; i < n; i++) {
+            Student s = array.get(i);
+            s.show();
+        }
+    }
+
+    public static void addStudent(ArrayList<Student> array) {
+        Scanner sc = new Scanner(System.in);
+        Student s = new Student();
+        System.out.println("\n输入学生的姓名：");
+        s.SetName(sc.nextLine());
+        System.out.println("\n输入学生的成绩：");
+        s.SetGrade(sc.nextInt());
+        array.add(s);*/
+
+
+    }
+}
